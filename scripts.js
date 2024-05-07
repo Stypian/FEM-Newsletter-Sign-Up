3cd3f5ac03deb968c832490fa505fd991c6348e5
Stypian <157755550+Stypian@users.noreply.github.com>
2024-05-06 20:24:04 -0400

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -9,13 +9,13 @@ onload = function() {
         const subThanksDesk = document.querySelector(".newsletter__submit--thanks");
         const subThanksMob = document.querySelector(".newsletter__submit--thanks-mobile");
         const emailError = document.querySelector(".newsletter__email-p--error");
-        const thanksButts = [subThanksDesk, subThanksMob];
+        const thanksButtons = [subThanksDesk, subThanksMob];
         const emailSpan = document.querySelectorAll(".newsletter__thanks-span");
         const valid = /^([a-zA-Z0-9\.-]+)@([a-zA-Z0-9-]+).([a-z]{2,3})$/;
 
 
         //remove the confirmation message and show normal screen
-        for (let i = 0; i < thanksButts.length; i++) {    
+        for (let i = 0; i < thanksButtons.length; i++) {    
             thanksButts[i].addEventListener("click", function() {
                 thanks.style.display = "none";
                 cont.style.display = "flex";
@@ -32,13 +32,13 @@ onload = function() {
         }
  
 
-        //add error colors
+        //add error colors and message
         function addErrors() {    
             emailError.style.display = "block";
             inpt.setAttribute("style", "border-color: hsl(4, 100%, 67%); color: hsl(4, 100%, 67%); background-color: hsla(4, 100%, 67%, 0.2)");
         }
         
-        //remove error colors
+        //remove error colors and message
         function remErrors() {     
             emailError.style.display = "none";
             inpt.setAttribute("style", "border-color: hsla(231, 7%, 60%, 0.6); color: #000; background-color: #fff");
