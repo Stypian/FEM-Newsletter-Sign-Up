c3611b2c6ad0b0b4fc767e44a5af0ac1e2c9d2d1
Stypian <157755550+Stypian@users.noreply.github.com>
2024-05-06 20:06:03 -0400

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -9,28 +9,36 @@ onload = function() {
         const subThanksDesk = document.querySelector(".newsletter__submit--thanks");
         const subThanksMob = document.querySelector(".newsletter__submit--thanks-mobile");
         const emailError = document.querySelector(".newsletter__email-p--error");
-        const thanksButtons = [subThanksDesk, subThanksMob];
+        const thanksButts = [subThanksDesk, subThanksMob];
+        const emailSpan = document.querySelectorAll(".newsletter__thanks-span");
         const valid = /^([a-zA-Z0-9\.-]+)@([a-zA-Z0-9-]+).([a-z]{2,3})$/;
 
 
-        //remove the thanks message and show normal screen
+        //remove the confirmation message and show normal screen
         for (let i = 0; i < thanksButts.length; i++) {    
-            thanksButtons[i].addEventListener("click", function() {
+            thanksButts[i].addEventListener("click", function() {
                 thanks.style.display = "none";
                 cont.style.display = "flex";
                 inpt.value = '';
                 remErrors();
             })
         }
+
+        //set email for confirmation message
+        const thanksEmail = () => {
+            for (let i = 0; i<emailSpan.length; i++) {
+                emailSpan[i].innerText = inpt.value;
+            }
+        }
  
 
-        //add error colors and message
+        //add error colors
         function addErrors() {    
             emailError.style.display = "block";
             inpt.setAttribute("style", "border-color: hsl(4, 100%, 67%); color: hsl(4, 100%, 67%); background-color: hsla(4, 100%, 67%, 0.2)");
         }
         
-        //remove error colors and message
+        //remove error colors
         function remErrors() {     
             emailError.style.display = "none";
             inpt.setAttribute("style", "border-color: hsla(231, 7%, 60%, 0.6); color: #000; background-color: #fff");
@@ -48,13 +56,14 @@ onload = function() {
             inpt.style.color = '#000';
         })
 
-        //show thanks message or errors
+        //show confirmation message or errors
         submit[2].addEventListener("click", function(e) {
             e.preventDefault();
             if (valid.test(inpt.value)) {
                 submitForm();
                 cont.style.display = 'none';
                 thanks.style.display = 'block';
+                thanksEmail();
             } else {
                 addErrors();
             }         
@@ -65,6 +74,35 @@ onload = function() {
     submitEmail();
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
 
 
 
