c35ef0410f295d783c470acd9f0972b16d15a10e
Stypian <157755550+Stypian@users.noreply.github.com>
2024-05-11 22:43:45 -0400

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -11,7 +11,7 @@ onload = function() {
         const emailError = document.querySelector(".newsletter__email-p--error");
         const thanksButtons = [subThanksDesk, subThanksMob];
         const emailSpan = document.querySelectorAll(".newsletter__thanks-span");
-        const valid = /^([a-zA-Z0-9\.\-\\_]+)@([a-zA-Z0-9-]+).([a-z]{2,3})$/;
+        const valid = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
 
         //remove the confirmation message and show normal screen
